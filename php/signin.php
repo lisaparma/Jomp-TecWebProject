4cc63d34403a19b25f8b4a0345282af42a006ddf
BluePerception <sara-17@hotmail.it>
2017-12-08 14:53:28 +0100

    Oscurata password nel form

--- php/signin.php
+++ php/signin.php
@@ -34,10 +34,10 @@ echo "<form method='post' action='signin.php'> <!-- i titoletti vanno in label--
         <input type='text' id='username' name='Username' placeholder='Username'> <br />
         
         <label for='password'> Password </label> <br/>
-        <input type='text' id='password' name='Password' placeholder='Password'> <br />
+        <input type='password' id='password' name='Password' placeholder='Password'> <br />
 
         <label for='rippw'> Ripeti password </label> <br/>
-        <input type='text' id='rippw' name='RipPassword' placeholder='Password'> <br />
+        <input type='password' id='rippw' name='RipPassword' placeholder='Password'> <br />
         <br />
 
         <input type='submit' value='Registrati' name='submit'>
