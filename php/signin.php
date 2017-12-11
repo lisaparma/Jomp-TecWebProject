d282b8402e97de1aeda48412e2b4186bd8987533
BluePerception <sara-17@hotmail.it>
2017-12-11 17:55:44 +0100

    Aggiunto attribure required per i campi del form

--- php/signin.php
+++ php/signin.php
@@ -24,22 +24,22 @@ echo "<div id=form>
                 <input type='radio' id='utente' name='tiporegistrazione' value='utente'> <label for='utente'> Utente</label> <br/>
 
                 <label for='nome'> Nome: </label> <br/>
-                <input type='text' id='nome' name='Nome' placeholder='Nome'> <br /> 
+                <input type='text' id='nome' name='Nome' placeholder='Nome'required> <br /> 
                 
                 <label for='cognome'> Cognome </label> <br/>
-                <input type='text' id='cognome' name='Cognome' placeholder='Cognome'> <br />
+                <input type='text' id='cognome' name='Cognome' placeholder='Cognome' required> <br />
                 
                 <label for='email'> E-mail: </label> <br/>
-                <input type='text' id='email' name='Email' placeholder='Email' pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'> <br />        
+                <input type='text' id='email' name='Email' placeholder='Email' pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$' required> <br />        
                 
                 <label for='username'> Username: </label> <br/>
-                <input type='text' id='username' name='Username' placeholder='Username'> <br />
+                <input type='text' id='username' name='Username' placeholder='Username' required> <br />
                 
                 <label for='password'> Password: </label> <br/>
-                <input type='password' id='password' name='Password' placeholder='Password'> <br />
+                <input type='password' id='password' name='Password' placeholder='Password' required> <br />
 
                 <label for='rippw'> Ripeti password: </label> <br/>
-                <input type='password' id='rippw' name='RipPassword' placeholder='Password'> <br />
+                <input type='password' id='rippw' name='RipPassword' placeholder='Password' required> <br />
                 <br />
 
                 <input type='submit' value='Registrati' name='submit'>
@@ -93,34 +93,22 @@ if(isset($_POST["submit"])){
         $Email=$_POST["Email"];
 
         //verifico i dati inseriti
-       
-        if ($Username || $Password || $RipPassword|| $Nome || $Cognome || $Email) {
+        if($Password===$RipPassword) {
 
+            $sql = "INSERT INTO Utenti(Username, Password, Nome, Cognome, Email) VALUES('$Username', '$Password', '$Nome', '$Cognome', '$Email')";
 
-
-
-            if($Password===$RipPassword) {
-
-                $sql = "INSERT INTO Utenti(Username, Password, Nome, Cognome, Email) VALUES('$Username', '$Password', '$Nome', '$Cognome', '$Email')";
-
-                if($db->query($sql)) {
-                    header("location: UtDashboard.php");
-                }
-                else{
-                    echo"<script type= 'text/javascript'>alert('Dati non inseriti cotterramente.');</script>";
-                }
+            if($db->query($sql)) {
+                header("location: UtDashboard.php");
             }
-            else {
-                echo "<script type= 'text/javascript'>alert('La password scelta deve essere uguale in entrambi i campi');</script>";
+            else{
+                echo"<script type= 'text/javascript'>alert('Dati non inseriti cotterramente.');</script>";
             }
         }
         else {
-            echo "<script type= 'text/javascript'>alert('Inserisci tutti i dati per continuare la registrazione');</script>";
+            echo "<script type= 'text/javascript'>alert('La password scelta deve essere uguale in entrambi i campi');</script>";
         }
-        
-
-
         closeDB($db);
+
 
     } catch (PDOException $e) {
         echo "Errore: " . $e->getMessage();
