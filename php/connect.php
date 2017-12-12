ad2842862bfd46c1ff9d91508e31c4b57a28e307
BluePerception <sara-17@hotmail.it>
2017-12-12 10:41:05 +0100

    Funzioni di check ok e modificato tipo di connessione in MySQLi

--- php/connect.php
+++ php/connect.php
@@ -5,11 +5,20 @@ function openDB() {
     $dbname 	= "database";
     $user 		= "root";
     $pass 		= "";
+
+    $con = mysqli_connect($hostname, $user, $pass, $dbname);
+
+    if(mysqli_connect_errno()) {
+    	echo "Connessione fallita con MySqL: ".mysqli_connect_error();
+    }
+
+    /*
     $db 		= new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	*/
+    return $con;
 
-    return $db;
 }
 
 function closeDB($db) {
--- php/signin.php
+++ php/signin.php
@@ -14,6 +14,50 @@ headers();
 
 menuHome();
 
+//inizializzo gli errori come una stringa vuota
+$emailErr = $usernameErr = $rippwErr = "";
+
+function checkEmail($Email) {
+    echo "checkEmail";
+    //$db = openDB();
+    $result = mysqli_query(openDB(),"SELECT Email FROM Utenti WHERE Email='".$Email."'");     //creo la query
+    
+    $num_rows = mysqli_num_rows($result);                                      //invio la query
+
+
+    if($num_rows != 0) {                                                 //resultato non vuoto: email già esistente
+        $emailErr = "Email già presente, controlla di non essere già registrato";
+        return false;
+    }
+    return true;
+}
+
+function checkUsername($Username) {
+    echo "checkUsername";
+
+    $result = mysqli_query(openDB(),"SELECT Username FROM Utenti WHERE Username='".$Username."'");
+
+    $num_rows = mysqli_num_rows($result);
+
+    if($num_rows != 0) {
+        $usernameErr = "Username non disponibile";
+        return false;
+    }
+    return true;
+}
+
+function checkRepeatPassword($Password, $RipPassword) {
+    echo "checkPass";
+    if($Password != $RipPassword) {
+        $rippwErr = "La password ripetuta non corrisponde alla precedente";
+        return false;
+    }
+    return true;
+}
+
+
+
+
 //Mettere i tab index nei form e nei link
 echo "<div id=form>
         <div id=contentForm>
@@ -24,22 +68,22 @@ echo "<div id=form>
                 <input type='radio' id='utente' name='tiporegistrazione' value='utente'> <label for='utente'> Utente</label> <br/>
 
                 <label for='nome'> Nome: </label> <br/>
-                <input type='text' id='nome' name='Nome' placeholder='Nome'required> <br /> 
+                <input type='text' id='nome' name='Nome' placeholder='Nome'required><br /> 
                 
                 <label for='cognome'> Cognome </label> <br/>
-                <input type='text' id='cognome' name='Cognome' placeholder='Cognome' required> <br />
+                <input type='text' id='cognome' name='Cognome' placeholder='Cognome' required><br />
                 
                 <label for='email'> E-mail: </label> <br/>
-                <input type='text' id='email' name='Email' placeholder='Email' pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$' required> <br />        
+                <input type='text' id='email' name='Email' placeholder='Email' pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$' required><span class='error'>$emailErr</span><br />        
                 
                 <label for='username'> Username: </label> <br/>
-                <input type='text' id='username' name='Username' placeholder='Username' required> <br />
+                <input type='text' id='username' name='Username' placeholder='Username' required><span class='error'>$usernameErr</span><br />
                 
                 <label for='password'> Password: </label> <br/>
-                <input type='password' id='password' name='Password' placeholder='Password' required> <br />
+                <input type='password' id='password' name='Password' placeholder='Password' required><br />
 
                 <label for='rippw'> Ripeti password: </label> <br/>
-                <input type='password' id='rippw' name='RipPassword' placeholder='Password' required> <br />
+                <input type='password' id='rippw' name='RipPassword' placeholder='Password' required><span class='error'>$rippwErr</span><br />
                 <br />
 
                 <input type='submit' value='Registrati' name='submit'>
@@ -47,37 +91,6 @@ echo "<div id=form>
             </form>
         </div>
     </div>";
-/*
-function checkEmail($Email) {
-
-    $query = "SELECT Email FROM Utenti WHERE Email='".$Email."'";     //creo la query
-
-    $result = mysql_query(query);                                     //invio la query
-
-    if($result != 0) {                                                 //resultato non vuoto: email già esistente
-        echo "Email già inserita, controlla di non essere già registrato.";
-        die();
-    }
-}
-
-function checkUsername($Username) {
-
-    $query = "SELECT Username FROM Utenti WHERE Username='".$Username"'";
-
-    $result = mysql_query(query);
-
-    if($result != 0) {
-        echo "Username non disponibile.";
-        die();
-    }
-}
-
-function checkRepeatPassword($Password, $RipPassword) {
-
-    if($Password != $RipPassword) {
-        echo "La password scelta deve essere uguale in entrambi i campi";
-    }
-}*/
 
 
 if(isset($_POST["submit"])){
@@ -93,20 +106,49 @@ if(isset($_POST["submit"])){
         $Email=$_POST["Email"];
 
         //verifico i dati inseriti
-        if($Password===$RipPassword) {
-
+        if(checkEmail($Email) && checkUsername($Username) && checkRepeatPassword($Password, $RipPassword)) {
             $sql = "INSERT INTO Utenti(Username, Password, Nome, Cognome, Email) VALUES('$Username', '$Password', '$Nome', '$Cognome', '$Email')";
 
-            if($db->query($sql)) {
-                header("location: UtDashboard.php");
-            }
-            else{
-                echo"<script type= 'text/javascript'>alert('Dati non inseriti cotterramente.');</script>";
-            }
+            $db->query($sql);
+            header("location: UtDashboard.php");
+
         }
         else {
-            echo "<script type= 'text/javascript'>alert('La password scelta deve essere uguale in entrambi i campi');</script>";
+            header("location: signin.php");
+            echo "<div id=form>
+                <div id=contentForm>
+                    <form method='post' action='signin.php'> 
+                    
+                        Come vuoi registrarti? <br />
+                        <input type='radio' id='azienda' name='tiporegistrazione' value='azienda' checked> <label for='azienda'>Azienda</label>
+                        <input type='radio' id='utente' name='tiporegistrazione' value='utente'> <label for='utente'> Utente</label> <br/>
+
+                        <label for='nome'> Nome: </label> <br/>
+                        <input type='text' id='nome' name='Nome' value=$Nome required><br /> 
+                        
+                        <label for='cognome'> Cognome </label> <br/>
+                        <input type='text' id='cognome' name='Cognome' value=$Cognome required><br />
+                        
+                        <label for='email'> E-mail: </label> <br/>
+                        <input type='text' id='email' name='Email' value=$Email pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$' required><span class='error'>$emailErr</span><br />        
+                        
+                        <label for='username'> Username: </label> <br/>
+                        <input type='text' id='username' name='Username' value=$Username required><span class='error'>$usernameErr</span><br />
+                        
+                        <label for='password'> Password: </label> <br/>
+                        <input type='password' id='password' name='Password' placeholder='Password' required><br />
+
+                        <label for='rippw'> Ripeti password: </label> <br/>
+                        <input type='password' id='rippw' name='RipPassword' placeholder='Password' required><span class='error'>$rippwErr</span><br />
+                        <br />
+
+                        <input type='submit' value='Registrati' name='submit'>
+
+                    </form>
+                </div>
+            </div>";
         }
+
         closeDB($db);
 
 
