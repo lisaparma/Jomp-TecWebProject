ab93e7b4dd550c092f78f7409b367adce4f79ed9
Lisa Parma <lisa.parma@hotmail.it>
2017-12-16 22:18:00 +0100

    Aggiunti controlli per sfatare errori stronzi

--- php/functionUtente.php
+++ php/functionUtente.php
@@ -112,19 +112,39 @@ function adsSalved()
 }
 
 
+function checkEmail($email) {
+    $result = mysqli_query(openDB(),"SELECT Username FROM Utenti WHERE Email='$email'");
+    $num_rows = mysqli_num_rows($result);
+    if($num_rows == 0) {
+        return true;
+    }
+    return false;
+}
+
+function checkUsername($username) {
+    $result = mysqli_query(openDB(),"SELECT * FROM Utenti WHERE Username='$username'");
+    $num_rows = mysqli_num_rows($result);
+    if($num_rows == 0) {
+        return true;
+    }
+    return false;
+    
+}
+
+
 function editData() {
     
 if(isset($_SESSION['login'])) {  
-    $nome=$_SESSION["login"]['Nome'];
-    $cognome=$_SESSION["login"]['Cognome'];
+    $name=$_SESSION["login"]['Nome'];
+    $surname=$_SESSION["login"]['Cognome'];
     $email=$_SESSION["login"]['Email'];
     $username=$_SESSION["login"]['Username'];
     $password=$_SESSION["login"]['Password'];
-    $sesso=$_SESSION["login"]['Sesso'];
+    $sex=$_SESSION["login"]['Sesso'];
     $check1='';
     $check2='';
 
-    if($sesso==='m') 
+    if($sex==='m') 
         $check1='checked';
     else 
         $check2='checked';
@@ -135,16 +155,16 @@ if(isset($_SESSION['login'])) {
 	            <form method='post' action='UtModificaDati.php'> 
 
 	                <label for='nome'> Nome: </label> <br/>
-	                <input type='text' id='nome' value='$nome' name='Nome'><br /> 
+	                <input type='text' id='nome' value='$name' name='Nome'><br /> 
 	                
 	                <label for='cognome'> Cognome </label> <br/>
-	                <input type='text' id='cognome' value='$cognome' name='Cognome'><br />
+	                <input type='text' id='cognome' value='$surname' name='Cognome'><br />
 	                
 	                <label for='email'> E-mail: </label> <br/>
-	                <input type='text' readonly='readonly' id='email' value='$email' name='Email'><br />        
+	                <input type='text' id='email' value='$email' name='Email'><br />        
 	                
 	                <label for='username'> Username: </label> <br/>
-	                <input type='text' readonly='readonly' id='username' value='$username' name='Username' value=''>
+	                <input type='text' id='username' value='$username' name='Username' value=''>
 	                <br />
 	                
 	                <label for='password'> Password: </label> <br/>
@@ -163,19 +183,48 @@ if(isset($_SESSION['login'])) {
     if(isset($_POST['modifica'])){
         
         try {
-            $Password1 = $_POST['Password'];
-            $Nome1 = $_POST['Nome'];
-            $Cognome1 = $_POST['Cognome'];
-            $Sesso1 = $_POST['button'];
+            $newName = $_POST['Nome'];
+            $newSurname = $_POST['Cognome'];
+            $newEmail = $_POST['Email'];
+            $newUsername= $_POST['Username'];
+            $newPassword = $_POST['Password'];
+            $newSex = $_POST['button'];
+            if($newUsername == $username) {
+                if(checkEmail($newEmail)){
+                    $sql = "UPDATE Utenti SET Nome='$newName', Cognome='$newSurname',Sesso='$newSex', Email='$newEmail', Password='$newPassword' WHERE Username='$username'";
+                }
+                else {
+                    echo "ERRORE EMAIL NON DISPONIBILE";
+                }
+            }
+            else{
+                if($newEmail == $email) {
+                    echo checkUsername($newUsername);
+                    if(checkUsername($newUsername)==1){
+                        $sql = "UPDATE Utenti SET Nome='$newName', Cognome='$newSurname', Sesso='$newSex', Username='$newUsername', Password='$newPassword'  WHERE Email='$email'";
+                    }
+                    else {
+                        echo "ERRORE USERNAME NON DISPONIBILE";
+                    }
+                } 
+                else {
+                    echo"NON PUOI CAMBIARE SIA USERNAME CHE EMAIL STRONZO";
+                }
+            }
+            
+            if(mysqli_query(openDB(), $sql)) {
+                $_SESSION["login"]['Nome']=$newName;
+                $_SESSION["login"]['Cognome']=$newSurname;
+                $_SESSION["login"]['Email']=$newEmail;
+                $_SESSION["login"]['Username']=$newUsername;
+                $_SESSION["login"]['Password']=$newPassword;
+                $_SESSION["login"]['Sesso']=$newSex;
+                header("location: UtModificaDati.php");
+            }
+            else {
+                echo "Errore nell'aggiornare i propri dati.";
+            }
 
-            $sql = "UPDATE Utenti SET Nome='$Nome1', Cognome='$Cognome1', Password='$Password1', Sesso='$Sesso1' WHERE Username='$username'";
-            global $db;
-            $db -> query($sql);
-            $_SESSION["login"]['Nome']=$Nome1;
-            $_SESSION["login"]['Cognome']=$Cognome1;
-            $_SESSION["login"]['Password']=$Password1;
-            $_SESSION["login"]['Sesso']=$Sesso1;
-            header("location: UtModificaDati.php");
         } 
         catch (Exception $e) {
             echo "Errore: " . $e->getMessage();
