c94b63f71d66eedeb8eed3b7b78c61440bd3b6ee
Lisa Parma <lisa.parma@hotmail.it>
2018-01-25 11:33:00 +0100

    Aziende partner styyylleeeee

--- php/sectionAziendePartner.php
+++ php/sectionAziendePartner.php
@@ -20,35 +20,31 @@ function getCompany() {
 	$result = mysqli_query(openDB(), "SELECT Nome, Citta, Email, Descrizione FROM Aziende ORDER BY Nome ASC");
 
 	if(mysqli_num_rows($result) == 0) {
-		echo "Nessuna azienda si è ancora registrata.";
+		echo "<div class='NoData'> Nessuna azienda si è ancora registrata. </div>";
 	}
 
 	else {
 		while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-			echo "<div id='section'>
-					<dl>
-						<dt><strong>".$row['Nome']."</strong></dt><br/>
-							<dd>Sede a ".$row['Citta']."</dd></br>
-							<dd>Contatto: ".$row['Email']."<dd></br>
-							<dd>".$row['Descrizione']."</dd></br>
-						</dt>
-					</dl>
-				</div>
-			<br/>";
+			echo "<div class='sectionCompany'>
+					<h3>".$row['Nome']."</strong></h3>
+					<ul>
+						<li> <strong>Sede:</strong> ".$row['Citta']."</li>
+						<li> <strong>Contatto:</strong> ".$row['Email']."</li>
+						<li> <strong>Sito web:</strong> Quando ci sarà verrà fuori</li>
+						<li>".$row['Descrizione']."</li>
+					</ul>
+					<hr>
+				</div>";
 		}
 
 	}
 }
 
-echo "<div id='section'><h2>Aziende partner</h2>
-		<p>Moltissime aziende appartenenti ai diversi settori lavorativi sono alla continua ricerca di
-		 personale. La nostra ampia raccolta di annunci ti permette di valutare quale lavoro sia più 
-		 adatto alle tue esigenze e alle tue passioni. Qui potrai conoscere meglio le aziende 
-		 proponenti e in che ambiente lavorativo ti si potrebbe presentare davanti.</p>
-		 <p>L'ordine in cui vengono presentate le aziende, segue quello alfabetico (non è stato
-		  applicato nessun criterio sulla valutazione dell'azienda o sul numero di annunci più
-		  seguiti dagli utenti).</p>
-		</div>";
+echo "<div id='intro'>
+		<h2>Aziende partner</h2>
+		<p>Moltissime aziende appartenenti ai diversi settori lavorativi sono alla continua ricerca di personale. La nostra ampia raccolta di annunci ti permette di valutare quale lavoro sia più adatto alle tue esigenze e alle tue passioni. Qui potrai conoscere meglio le aziende proponenti e in che ambiente lavorativo ti si potrebbe presentare davanti.</p>
+		<p>L'ordine in cui vengono presentate le aziende, segue quello alfabetico (non è stato applicato nessun criterio sulla valutazione dell'azienda o sul numero di annunci più seguiti dagli utenti).</p>
+	</div>";
 getCompany();
 
 
