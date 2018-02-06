3354bbfd432ae4df81a3a4f0b6220e1cf7805318
BluePerception <sara-17@hotmail.it>
2018-02-06 15:36:30 +0100

    Sistemata Dashboard Admin e aggiornato database e popolamento

--- php/AdminModUtenti.php
+++ php/AdminModUtenti.php
@@ -20,6 +20,40 @@ menu($page);
 
 # ------------------------------------------------------
 
+echo"<div id='contenuto'>";
+
+$result = mysqli_query(openDB(), "SELECT * FROM Utenti WHERE Uso='utente' ORDER BY Cognome");
+
+if(mysqli_num_rows($result) == 0) {
+	echo "Nessun utente ancora registrato.";
+}
+
+else {
+	echo "<h3>Ecco gli utenti che si sono registrati:</h3>
+		<ul>";
+	
+	while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+		echo "<li class=''>
+				<ul>
+					<li id='title'>Nome: ".$row['Cognome']." ".$row['Nome'].";</li>
+					<li>Data di nascita: ".$row['Nascita'].";</li>
+					<li>Email: ".$row['Email'].";</li>
+					<li>Iscritto il ".$row['Iscrizione'].";</li>
+					<li>Username scelto: ".$row['Username'].";</li>
+					
+						<div id='options'>
+							<form method='post' action='AdminModUtenti.php'>
+				            	<button value=".$row['Username']." name='delete'>Rimuovi</button>
+			            	</form>
+		            	</div>
+				</ul>
+			</li>";
+	}
+	echo"</ul>";
+
+}
+
+
 
 
 
--- php/functionHome.php
+++ php/functionHome.php
@@ -88,5 +88,5 @@ function printAdsHome($result) { //se si modifica questa cambiare anche printAds
 				</li>"
                 ;
 		}	
-}
+	}
 ?>
